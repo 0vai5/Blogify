4a596f2aa23da29400d6f5af9c84874c4de290f7
Ovais Raza <rovais53@gmail.com>
2024-11-02 22:14:56 +0500

    Add blog details and layout to Blog component

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,8 +1,32 @@
-import react from "react";
+import react, { useState } from "react";
 
 const Blog = () => {
+    const [blog, setBlog] = useState({
+        Image: "https://i.ytimg.com/vi/jPl34SsWgq4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCqRiSqAkXB7BlEtJXAnY6Wh3FNLQ",
+        title: "React and Snity",
+        author: "Ovais Raza",
+        content: "hey it is a long blog and it can be done like the way i can doo as i like"
+    })
     return (
-        <div>Blog</div>
+        <section className="max-container">
+            <div>
+                <img 
+                src={blog.Image} 
+                alt={blog.title}
+                height={"200px"}
+                width={"100%"}
+                className="bg-contain bg-center"
+                />
+            </div>
+            <div>
+                <h1 className="subhead-text">{blog.title}</h1>
+            </div>
+            <div className="flex justify-between items-center">
+                <p>{blog.author}</p>
+                <p>DAte</p>
+            </div>
+            <div>{blog.content}</div>
+        </section>
     )
 }
 
