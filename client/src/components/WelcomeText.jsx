868e60325221bc0e4969b2ee6f01bd513740b204
Ovais Raza <rovais53@gmail.com>
2024-11-21 20:34:24 +0500

    Remove unused files and components; add CORS dependency to server; update app configuration

--- client/src/components/WelcomeText.jsx
+++
@@ -1,9 +0,0 @@
-import { GlobalContext } from "@/contexts/GlobalContext";
-import react, { useContext } from "react";
-
-const WelcomeText = () => {
-    const { user } = useContext(GlobalContext)
-    return user ? <h1 className="head-text">Welcome, {user}</h1> : ""
-}
-
-export default WelcomeText
