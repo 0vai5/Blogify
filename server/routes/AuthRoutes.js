63e155bc320894d6778034ea68f19c6e12ab4439
Ovais Raza <rovais53@gmail.com>
2025-01-09 00:37:12 +0500

    Remove server routes, models, controllers, and configuration files

--- server/routes/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { SignUP, SignIN, SignOut } = require('../controllers/AuthController');
-
-router.post('/signUP', SignUP);
-router.post('/signIN', SignIN);
-router.get('/signOUT', SignOut);
-
-module.exports = router;
--- server/routes/UserRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { UserDetails } = require('../controllers/UserController');
-
-router.post('/userDetail', UserDetails);
-
-module.exports = router;
